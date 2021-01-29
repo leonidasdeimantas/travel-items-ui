514abfa7d0f73ab0de570f82e7ede49ebce71a19
Leonidas <45066606+leonidasdeimantas@users.noreply.github.com>
2021-01-29 15:37:21 +0200

    working with api...

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ItemEnter from './components/ItemEnter'
 import Header from './components/Header'
 import './App.css';
 
+const API_URL = 'http://localhost:8080'
+
 class App extends React.Component  {
   constructor() {
     super()
@@ -11,7 +13,8 @@ class App extends React.Component  {
         items: [],
         item_cnt: 0,
         people: [],
-        page: "items"
+        page: "items",
+        tripUrl: "UkMQ3fuS"
     }
     this.handleAddItem = this.handleAddItem.bind(this)
     this.handleRemoveItem = this.handleRemoveItem.bind(this)
@@ -20,72 +23,89 @@ class App extends React.Component  {
     this.handleChangePage = this.handleChangePage.bind(this)
   }
 
-  handleAddItem(text) {
-    this.setState(prevState => {
-      let updatedItems = prevState.items;
-      let newItem = {
-        id: this.state.item_cnt,
-        text: text,
-        assignee: "",
-        price: "",
-        completed: false
-      }
-      updatedItems.push(newItem)
+  componentDidMount() {
+    this.fetchTrip()
+  }
 
-      return {
-        items: updatedItems,
-        item_cnt: prevState.item_cnt+1
+  async fetchTrip() {
+    const response = await fetch(`${API_URL}/tasks?tripUrl=${this.state.tripUrl}`)
+    const data = await response.json()
+    let taskItems = []
+
+    data.forEach(task => {
+      let newItem = {
+        id: task.id,
+        text: task.task,
+        assignee: task.assigneeId,
+        price: task.price,
+        completed: task.completed
       }
+      taskItems.push(newItem)
     })
+
+    this.setState({
+      items: taskItems.reverse(),
+      item_cnt: taskItems.length
+    })
+  }
+
+  handleAddItem(text) {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        task: text,
+        tripUrl: this.state.tripUrl
+      })
+    };
+    fetch(`${API_URL}/task`, requestOptions).then(() => { this.fetchTrip() });
   }
 
   handleRemoveItem(id) {
-    this.setState(prevState => {
-      let updatedItems = prevState.items;
-      let i = 0;
+    const requestOptions = {
+      method: 'DELETE'
+    };
+    fetch(`${API_URL}/task?tripUrl=${this.state.tripUrl}&taskId=${id}`, requestOptions).then(() => { this.fetchTrip() });
+  }
 
-      while (i < updatedItems.length) {
-        if (updatedItems[i].id === id) {
-          updatedItems.splice(i, 1)
-          break
-        } else {
-          ++i
-        }
-      }
-
-      return {
-        items: updatedItems
-      }
-    })
+  updateItem(task) {
+    const requestOptions = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        id: task.id,
+        task: task.text,
+        price: task.price,
+        assigneeId: task.assignee,
+        tripUrl: this.state.tripUrl,
+        completed: task.completed
+      })
+    };
+    console.log(task)
+    fetch(`${API_URL}/task`, requestOptions).then(() => { this.fetchTrip() });
   }
 
   handleDone(id) {
-    this.setState(prevState => {
-        const updatedItems = prevState.items.map(item => {
-            if (item.id === id) {
-                item.completed = !item.completed
-            }
-            return item
-        })
-        return {
-            items: updatedItems
-        }
+    let task = this.state.items.find(item => {
+      if (item.id === id) {
+        return item
+      }
     })
+
+    task.completed = !task.completed
+    this.updateItem(task)
   }
 
   handleEdit(id, assignee, price) {
-    this.setState(prevState => {
-        const updatedItems = prevState.items.map(item => {
-            if (item.id === id) {
-                item.assignee = assignee
-                item.price = price
-            }
-            return item
-        })
-        return {
-            items: updatedItems
-        }
+    let task = this.state.items.find(item => {
+      if (item.id === id) {
+        return item
+      }
     })
+
+    task.assignee = assignee
+    task.price = price
+    this.updateItem(task)
   }
 
   handleChangePage(value) {
@@ -94,6 +114,7 @@ class App extends React.Component  {
 
   render() {
     console.log(this.state.items)
+
     return (
       <div className="App">
           <Header 
@@ -105,17 +126,17 @@ class App extends React.Component  {
           {
             (this.state.page == "items") &&
               <main role="main" className="container">
-              <ItemEnter handleAddItem={this.handleAddItem} />
-              {
-                (this.state.items.length > 0) &&
-                <ItemList 
-                  items={this.state.items}
-                  handleDone={this.handleDone}
-                  handleEdit={this.handleEdit}
-                  handleRemoveItem={this.handleRemoveItem}
-                />
-              }
-            </main>
+                <ItemEnter handleAddItem={this.handleAddItem} />
+                {
+                  (this.state.items.length > 0) &&
+                  <ItemList 
+                    items={this.state.items}
+                    handleDone={this.handleDone}
+                    handleEdit={this.handleEdit}
+                    handleRemoveItem={this.handleRemoveItem}
+                  />
+                }
+              </main>
           }
       </div>
     );
