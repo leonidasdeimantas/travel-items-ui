bf798bc40da4ea8c903c9cafe7c5855895b1989c
Leonidas <45066606+leonidasdeimantas@users.noreply.github.com>
2021-03-04 15:36:55 +0200

    Update ItemEnter.js

--- src/components/ItemEnter.js
+++ src/components/ItemEnter.js
@@ -19,6 +19,10 @@ class ItemEnter extends React.Component {
         this.copyToClipboard(window.location.href)
     }
 
+    handleCopyID() {
+        this.copyToClipboard(this.props.tripUrl)
+    }
+
     copyToClipboard(text) {
         if (window.clipboardData && window.clipboardData.setData) {
             return window.clipboardData.setData("Text", text);
@@ -47,17 +51,21 @@ class ItemEnter extends React.Component {
             <div>
                 <br />
                 <blockquote className="blockquote text-center">
-                    <p className="mb-0 h5 text-dark">{this.props.tripName}<span className="text-info"></span></p>
-                    <footer className="font-weight-normal"><small>
-                        <a className="text-info" href="#" onClick={() => this.handleCopyUrl()}>
-                            Copy link
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" style={{ marginLeft: "5px", marginRight: "6px" }} className="bi bi-clipboard" viewBox="0 0 16 16">
-                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
-                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
-                            </svg>
-                        </a>
-              and share it with your friends
-          </small></footer>
+                    <p className="mb-0 h5 text-dark">{this.props.tripName}<span className="text-info"></span>
+                        <span className="dropdown" style={{ marginLeft: "10px", marginRight: "1px" }}>
+                            <button className="btn btn-outline-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Share
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" style={{ marginLeft: "5px", marginRight: "1px" }} className="bi bi-clipboard" viewBox="0 0 16 16">
+                                    <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+                                    <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+                                </svg>
+                            </button>
+                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a className="dropdown-item" href="#" onClick={() => this.handleCopyID()}>Copy ID</a>
+                                <a className="dropdown-item" href="#" onClick={() => this.handleCopyUrl()}>Copy link</a>
+                            </div>
+                        </span>
+                    </p>
                 </blockquote>
                 <form className="input-group mb-3 C5procTop" onSubmit={e => this.handleButton(e)}>
                     <input id="ItemEnterInputID" type="textbox" className="form-control border-white" placeholder={"Add " + this.props.item + "..."} ref={this.msgRef} />
