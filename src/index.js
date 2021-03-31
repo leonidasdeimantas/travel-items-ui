770a883e7e9691fb0fccf0f2fe410b3e41949676
Leonidas <45066606+leonidasdeimantas@users.noreply.github.com>
2021-03-31 21:39:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
     <Router>
-        <Route path='/:handle' component={App} />
+        <Route path='/' component={App} />
     </Router>,
     document.getElementById('root')
 );
