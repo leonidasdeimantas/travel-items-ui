9ee4e33cc70574279a9213ce39ab35316f6e6ab1
Leonidas <45066606+leonidasdeimantas@users.noreply.github.com>
2021-04-06 10:09:16 +0300

    ItemList.js class -> function

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,45 +1,42 @@
 import React from 'react';
 import Item from './Item'
 
-class ItemList extends React.Component {
-    render() {
-        const list_remaining = this.props.items.map(item => !item.completed && <Item
-            key={item.id}
-            item={item}
-            people={this.props.people}
-            handleDone={this.props.handleDone}
-            handleEdit={this.props.handleEdit}
-            handleRemoveItem={this.props.handleRemoveItem}
-        />)
+function ItemList(props) {
+    const list_remaining = props.items.map(item => !item.completed && <Item
+        key={item.id}
+        item={item}
+        people={props.people}
+        handleDone={props.handleDone}
+        handleEdit={props.handleEdit}
+        handleRemoveItem={props.handleRemoveItem}
+    />)
 
-        const list_completed = this.props.items.map(item => item.completed && <Item
-            key={item.id}
-            item={item}
-            people={this.props.people}
-            handleDone={this.props.handleDone}
-            handleEdit={this.props.handleEdit}
-            handleRemoveItem={this.props.handleRemoveItem}
-        />)
+    const list_completed = props.items.map(item => item.completed && <Item
+        key={item.id}
+        item={item}
+        people={props.people}
+        handleDone={props.handleDone}
+        handleEdit={props.handleEdit}
+        handleRemoveItem={props.handleRemoveItem}
+    />)
 
-        return (
-            <div>
-                {(!list_remaining.every(item => (item === false))) &&
-                    <div className="my-3 p-3 bg-white rounded box-shadow">
-                        <h6 className="border-bottom border-gray pb-2 mb-0">Remaining items</h6>
-                        {list_remaining}
-                    </div>
-                }
+    return (
+        <div>
+            {(!list_remaining.every(item => (item === false))) &&
+                <div className="my-3 p-3 bg-white rounded box-shadow">
+                    <h6 className="border-bottom border-gray pb-2 mb-0">Remaining items</h6>
+                    {list_remaining}
+                </div>
+            }
 
-                {(!list_completed.every(item => (item === false))) &&
-                    <div className="my-3 p-3 bg-white rounded box-shadow">
-                        <h6 className="border-bottom border-gray pb-2 mb-0">Completed items</h6>
-                        {list_completed}
-                    </div>
-                }
-
-            </div>
-        );
-    }
+            {(!list_completed.every(item => (item === false))) &&
+                <div className="my-3 p-3 bg-white rounded box-shadow">
+                    <h6 className="border-bottom border-gray pb-2 mb-0">Completed items</h6>
+                    {list_completed}
+                </div>
+            }
+        </div>
+    );
 }
 
 export default ItemList;
