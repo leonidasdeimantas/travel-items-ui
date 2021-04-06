aca223121ba3847aa5263111b42f697bcbba6862
Leonidas <45066606+leonidasdeimantas@users.noreply.github.com>
2021-04-06 09:12:24 +0300

    Header.js class -> function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,31 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-md fixed-top navbar-dark" style={{ backgroundColor: "#151515" }}>
-                    <a className="navbar-brand mx-auto order-0 CLogoText" href="# ">Travel items</a>
-                </nav>
-                {
-                    (this.props.tripFound) &&
-                    <div className="nav-scroller bg-white box-shadow">
-                        <nav className="nav nav-underline">
-                            <a className={"nav-link " + (this.props.page === "main" ? "active" : "")} href="# " onClick={() => this.props.handleChangePage("main")}>
-                                Home
-              </a>
-                            <a className={"nav-link " + (this.props.page === "items" ? "active" : "")} href="# " onClick={() => this.props.handleChangePage("items")}>
-                                Items
-                <span className="badge badge-pill bg-light align-text-bottom">{this.props.itemCnt}</span>
-                            </a>
-                            <a className={"nav-link " + (this.props.page === "people" ? "active" : "")} href="# " onClick={() => this.props.handleChangePage("people")}>
-                                People
-                <span className="badge badge-pill bg-light align-text-bottom">{this.props.peopleCnt}</span>
-                            </a>
-                        </nav>
-                    </div>
-                }
-            </div>
-        );
-    }
+function Header(props) {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-md fixed-top navbar-dark" style={{ backgroundColor: "#151515" }}>
+                <a className="navbar-brand mx-auto order-0 CLogoText" href="# ">Travel items</a>
+            </nav>
+            {
+                (props.tripFound) &&
+                <div className="nav-scroller bg-white box-shadow">
+                    <nav className="nav nav-underline">
+                        <a className={"nav-link " + (props.page === "main" ? "active" : "")} href="# " onClick={() => props.handleChangePage("main")}>
+                            Home
+                        </a>
+                        <a className={"nav-link " + (props.page === "items" ? "active" : "")} href="# " onClick={() => props.handleChangePage("items")}>
+                            Items
+                            <span className="badge badge-pill bg-light align-text-bottom">{props.itemCnt}</span>
+                        </a>
+                        <a className={"nav-link " + (props.page === "people" ? "active" : "")} href="# " onClick={() => props.handleChangePage("people")}>
+                            People
+                            <span className="badge badge-pill bg-light align-text-bottom">{props.peopleCnt}</span>
+                        </a>
+                    </nav>
+                </div>
+            }
+        </div>
+    );
 }
 
 export default Header;
