4d921810c05098cd54c7acded58bb0b9410e0aba
Leonidas <45066606+leonidasdeimantas@users.noreply.github.com>
2021-04-06 09:03:49 +0300

    Assignees.js classes -> functions

--- src/components/Assignees.js
+++ src/components/Assignees.js
@@ -1,31 +1,27 @@
 import React from 'react';
 
-class Assignees extends React.Component {
-    render() {
-        const list = this.props.people.map(item => <AssigneeItem
-            key={item.id}
-            people={item}
-            handleRemove={this.props.handleRemove}
-        />)
+function Assignees(props) {
+    const list = props.people.map(item => <AssigneeItem
+        key={item.id}
+        people={item}
+        handleRemove={props.handleRemove}
+    />)
 
-        return (
-            <div className="C5procTop d-flex flex-wrap">
-                {list}
-            </div>
-        );
-    }
+    return (
+        <div className="C5procTop d-flex flex-wrap">
+            {list}
+        </div>
+    );
 }
 
-class AssigneeItem extends React.Component {
-    render() {
-        return (
-            <div className="p-2">
-                <button className="btn btn-info" onClick={() => this.props.handleRemove(this.props.people.id)}>{this.props.people.name}
-                    <span style={{ marginLeft: "10px" }} aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        );
-    }
+function AssigneeItem(props) {
+    return (
+        <div className="p-2">
+            <button className="btn btn-info" onClick={() => props.handleRemove(props.people.id)}>{props.people.name}
+                <span style={{ marginLeft: "10px" }} aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    );
 }
 
 export default Assignees;
