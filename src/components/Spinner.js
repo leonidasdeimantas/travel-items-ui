633d642090794526845d9e413262c94621fe06b6
Leonidas <45066606+leonidasdeimantas@users.noreply.github.com>
2021-04-06 10:11:20 +0300

    Spinner.js class -> function

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
-class Spinner extends React.Component {
-    render() {
-        return (
-            this.props.loading &&
-            <div id="overlay">
-                <div className="spinner-border text-info" role="status">
-                    <span className="sr-only">Loading...</span>
-                </div>
+function Spinner(props) {
+    return (
+        props.loading &&
+        <div id="overlay">
+            <div className="spinner-border text-info" role="status">
+                <span className="sr-only">Loading...</span>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
 
 export default Spinner;
