7cc8784a5b52bbd26402c9a53cc575758ff6af36
Leonidas <45066606+leonidasdeimantas@users.noreply.github.com>
2021-04-06 10:01:36 +0300

    ItemEnter.js class -> function

--- src/components/ItemEnter.js
+++ src/components/ItemEnter.js
@@ -1,77 +1,67 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-class ItemEnter extends React.Component {
-    constructor() {
-        super()
-        this.msgRef = React.createRef()
-    }
+function ItemEnter(props) {
+    const msgRef = useRef(null)
 
-    handleButton(event) {
+    const handleButton = (event) => {
         event.preventDefault()
-        let text = this.msgRef.current.value
+        let text = msgRef.current.value
         if (text === "") return
-        this.props.handleAddItem(text)
-        this.msgRef.current.value = ""
+        props.handleAddItem(text)
+        msgRef.current.value = ""
         document.getElementById("ItemEnterInputID").focus()
     }
 
-    handleCopyUrl() {
-        this.copyToClipboard(window.location.href)
-    }
+    const handleCopyUrl = () => { copyToClipboard(window.location.href) }
+    const handleCopyID = () => { copyToClipboard(props.tripUrl) }
 
-    handleCopyID() {
-        this.copyToClipboard(this.props.tripUrl)
-    }
+    return (
+        <div>
+            <br />
+            <blockquote className="blockquote text-center">
+                <p className="mb-0 h5 text-dark">{props.tripName}<span className="text-info"></span>
+                    <span className="dropdown" style={{ marginLeft: "10px"}}>
+                        <button className="btn btn-outline-info btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span className="material-icons-outlined" style={{fontSize:"18px", marginRight:"4px"}}>share</span>
+                            Share
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a className="dropdown-item" href="# " onClick={() => handleCopyID()}>Copy ID <span className="material-icons-outlined" style={{fontSize:"18px", marginLeft:"4px"}}>content_copy</span></a>
+                            <a className="dropdown-item" href="# " onClick={() => handleCopyUrl()}>Copy link <span className="material-icons-outlined" style={{fontSize:"18px", marginLeft:"4px"}}>content_copy</span></a>
+                        </div>
+                    </span>
+                </p>
+            </blockquote>
+            <form className="input-group mb-3 C5procTop" onSubmit={e => handleButton(e)}>
+                <input id="ItemEnterInputID" type="textbox" className="form-control border-white" placeholder={"Add " + props.item + "..."} ref={msgRef} />
+                <div className="input-group-append">
+                    <input type="submit" className="btn btn-outline-secondary" value="Add" />
+                </div>
+            </form>
+        </div>
+    );
+}
 
-    copyToClipboard(text) {
-        if (window.clipboardData && window.clipboardData.setData) {
-            return window.clipboardData.setData("Text", text);
+function copyToClipboard(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+        return window.clipboardData.setData("Text", text);
+    }
+    else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");
         }
-        else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-            var textarea = document.createElement("textarea");
-            textarea.textContent = text;
-            textarea.style.position = "fixed";
-            document.body.appendChild(textarea);
-            textarea.select();
-            try {
-                return document.execCommand("copy");
-            }
-            catch (ex) {
-                console.warn("Copy to clipboard failed.", ex);
-                return false;
-            }
-            finally {
-                document.body.removeChild(textarea);
-            }
+        catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
         }
-    }
-
-    render() {
-        return (
-            <div>
-                <br />
-                <blockquote className="blockquote text-center">
-                    <p className="mb-0 h5 text-dark">{this.props.tripName}<span className="text-info"></span>
-                        <span className="dropdown" style={{ marginLeft: "10px"}}>
-                            <button className="btn btn-outline-info btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span className="material-icons-outlined" style={{fontSize:"18px", marginRight:"4px"}}>share</span>
-                                Share
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a className="dropdown-item" href="# " onClick={() => this.handleCopyID()}>Copy ID <span className="material-icons-outlined" style={{fontSize:"18px", marginLeft:"4px"}}>content_copy</span></a>
-                                <a className="dropdown-item" href="# " onClick={() => this.handleCopyUrl()}>Copy link <span className="material-icons-outlined" style={{fontSize:"18px", marginLeft:"4px"}}>content_copy</span></a>
-                            </div>
-                        </span>
-                    </p>
-                </blockquote>
-                <form className="input-group mb-3 C5procTop" onSubmit={e => this.handleButton(e)}>
-                    <input id="ItemEnterInputID" type="textbox" className="form-control border-white" placeholder={"Add " + this.props.item + "..."} ref={this.msgRef} />
-                    <div className="input-group-append">
-                        <input type="submit" className="btn btn-outline-secondary" value="Add" />
-                    </div>
-                </form>
-            </div>
-        );
+        finally {
+            document.body.removeChild(textarea);
+        }
     }
 }
 
